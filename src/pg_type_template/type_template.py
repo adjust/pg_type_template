70e768205986d2b96585a74e068f45065a9a8f0b
Artur Zakirov <zaartur@gmail.com>
2023-02-24 12:10:36 +0100

    Add check of enum values (#9)

--- src/pg_type_template/type_template.py
+++ src/pg_type_template/type_template.py
@@ -5,23 +5,45 @@ import shutil
 from jinja2 import Environment, PackageLoader
 
 def sort_by_name(value):
+    """
+    Jinja filter which sorts C macros by their names.
+    """
     return sorted(value, key=lambda t: t["name"])
 
-def sort_by_value(value):
-    return sorted(value, key=lambda t: t["value"])
-
 def uint8_name(value):
+    """
+    Jinja filter which generates C macro name.
+    """
     return value.upper().replace(" ", "_").replace("-", "_")
 
 class TypeTemplate:
+    """
+    A class that generates PostgreSQL extensions which adds new enum data type.
+    """
     def __init__(self, model):
+        self.check_values(model)
+
         self.model = model
         self.env = Environment(loader=PackageLoader(__package__), trim_blocks=True)
         self.env.filters["uint8_name"] = uint8_name
         self.env.filters["sort_by_name"] = sort_by_name
-        self.env.filters["sort_by_value"] = sort_by_value
+
+    def check_values(self, model):
+        """
+        Check Postgresql extension model values.
+
+        The function raises ValueError exception if any enum value is outside of
+        allowed range.
+        """
+        for model_type in model["types"]:
+            for type_values in model_type["type_values"]:
+                if type_values["value"] < 0 or type_values["value"] > 255:
+                    raise ValueError(f"value '{type_values['value']}' of '{type_values['name']}' is out of range")
 
     def render_to_dir(self, dest_dir):
+        """
+        Generate new PostgreSQL extension into dest_dir directory.
+        """
         for template_name in self.env.list_templates():
             template = self.env.get_template(template_name)
 
